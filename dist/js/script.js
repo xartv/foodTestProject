32cdba086d5fb614c980d8640aea436524eb6b78
xartv <xvasartx@gmail.com>
2022-10-02 19:03:20 +0400

    jsed timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,6 +97,7 @@
 
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabsWrapper = document.querySelector('.tabheader__items');
@@ -131,7 +132,57 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  });
+  }); // Timer
+
+  let deadline = '2022-10-10';
+
+  function calculateDifferenceOfDate(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(t / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(t / 1000 / 60 % 60),
+          seconds = Math.floor(t / 1000 % 60);
+    return {
+      t,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setTimer(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timerID = setInterval(updateTimer, 1000);
+    updateTimer();
+
+    function updateTimer() {
+      const t = calculateDifferenceOfDate(endtime);
+
+      if (t.t <= 0) {
+        clearInterval(timerID);
+      }
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+    }
+  }
+
+  setTimer('.timer', deadline);
 });
 
 /***/ })
