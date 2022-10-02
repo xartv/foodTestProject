01126a7c2944eed7b5ffcbbef36e3a5b838a931e
xartv <xvasartx@gmail.com>
2022-10-02 21:32:12 +0400

    modified timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -137,11 +137,18 @@ window.addEventListener('DOMContentLoaded', () => {
   let deadline = '2022-10-10';
 
   function calculateDifferenceOfDate(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 * 24)),
-          hours = Math.floor(t / (1000 * 60 * 60) % 24),
-          minutes = Math.floor(t / 1000 / 60 % 60),
-          seconds = Math.floor(t / 1000 % 60);
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = hours = minutes = seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor(t / (1000 * 60 * 60) % 24);
+      minutes = Math.floor(t / 1000 / 60 % 60);
+      seconds = Math.floor(t / 1000 % 60);
+    }
+
     return {
       t,
       days,
